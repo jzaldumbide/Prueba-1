fcf75705a920d008c2606d0a583d67dfd699dcb3
KatherineCriollo <Kathy_eliza_1994@hotmail.com>
2016-05-19 07:22:52 -0500

    Ejercicio uno
    Encabezado

---
+++ ejercicio_uno.py
@@ -0,0 +1,9 @@
+#Punto numero uno 
+# Suma de vectores x,y,z
+#Encabezado
+print("Escuela Politecnica Nacional")
+print("Escuela de Formacion de Tecnologos")
+print("Programacion Avanzada")
+print("Prueba de Programación Avanzada")
+print("Bienvenido a su porgrama de conteo de palabras")
+print("")
