ffb73ce693374166237e92a1518b0cec9d2ce447
Michelle Arias <michu.arias17@hotmail.es>
2016-05-19 07:29:27 -0500

    Menu

---
+++ Prueba.py
@@ -0,0 +1,24 @@
+# Prueba Primer Bimestre
+# Arias Michelle, Arias Karla, Criollo Katherine, Rommel Guaman
+
+print("Escuela Politecnica Nacional")
+print("Escuela de Formacion de Tecnologos")
+print("Programacion Avanzada")
+
+print ("Programa Prueba Primer Bimestre\n")
+print ("-1- Suma de Vectores x,y,z")
+print ("-2- Promedio")
+print ("-3- Division e Inversion de un Archivo de Texto\n")
+x= int(input("Ingrese la opcion que desee realizar: "))
+
+if(x==1): 
+	print ("Bienvenido a la Suma de Vectores")
+
+elif(x==2):
+	print ("Bienvenido al Promedio")
+
+elif(x==3):
+	print ("Bienvenido a la Division e Inversion de un Archivo de Texto")
+
+else: 
+	print ("Gracias por utilizar el programa")
